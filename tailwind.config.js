997cd6c1b0659a5e3218dfe23a70b9af92667057
Nisanureser <Nisa2415@hotmail.com>
2024-09-19 21:31:15 +0300

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: "selector",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "12rem",
+    },
+    extend: {
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD369",
+        "gega-grey": "#DDDDDD",
+        "gega-white": "#F7F7F7",
+      },
+      spacing: {
+        128: "32rem",
+      },
+    },
   },
   plugins: [],
 };
